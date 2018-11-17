8e5b2ad7750d6e480106628a5c56266bf4898bb4
MOHAMMED ARSHAD <arshad0141786@gmail.com>
2018-11-17 13:01:52 +0530

    tutorials done

--- src/pages/tutorials/tutorials.ts
+++ src/pages/tutorials/tutorials.ts
@@ -17,24 +17,24 @@ export class TutorialsPage {
 
   tutorials = [
     {
-      title: "Find everything",
-      description: "Find all necessary <b>engineering</b> accessories",
-      image: "assets/imgs/tutorials/t4.png",
+      title: "Welcome to XIBAY",
+      description: "Xibay - E<b>X</b>change <b>I</b>n <b>B</b>etter w<b>AY</b>.<br/>Find everything you need for your <b>engineering</b>",
+      image: "assets/imgs/tuts/first.jpg",
     },
     {
-      title: "Help others",
-      description: "Exchange goodies in a better way",
-      image: "assets/imgs/tutorials/t2.png",
+      title: "Get Products for Free",
+      description: "Send requests for any product and get<br/>notified when other user accept it.<br/><b>By just one click</b>",
+      image: "assets/imgs/tuts/happy.jpg",
     },
     {
-      title: "Third ",
-      description: "This is the <b>third page</b> of tutorial page",
-      image: "assets/imgs/tutorials/t1.png",
+      title: "Post your products",
+      description: "You can post your products related to engineering which are useless for you but useful for others.<br/>Help others and get <b>blessed</b>.",
+      image: "assets/imgs/tuts/good.jpg",
     },
     {
-      title: "Last Tutorials",
-      description: "This is the Second page of tutorial page",
-      image: "assets/imgs/tutorials/t3.png",
+      title: "We gurantee for security",
+      description: "<b>XIBAY</b> team always look forward for the security and easiness of their family.<br/>Trust us and play we <b>care</b> for you.",
+      image: "assets/imgs/tuts/secure.jpg",
     }
   ];
   constructor(

e849991decf29e88336b62c54c35655fe1f92225
MOHAMMED ARSHAD <arshad0141786@gmail.com>
2018-11-17 20:35:44 +0530

    registration page done

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -66,7 +66,6 @@ export class RegistrationPage {
     private logs: LogsServiceProvider) {
       this.signupOptions.phone_number = this.navParams.get('phone');
       console.log('phone number : ' + this.signupOptions.phone_number);
-      this.notify.presentLoading("Please wait...");
   }
 
   ionViewDidLoad() {
@@ -128,69 +127,4 @@ export class RegistrationPage {
       
     }
   }
-
-
-  registerUser(value: any){
-    // this.logs.addLog("Register user : "+JSON.stringify(this.defaultFormData));
-    // this.logs.addLog("Register user : "+JSON.stringify(value));
-    // //this.networkEngine.post('/create-user-for-xibay',this.defaultFormData)
-    // this.networkEngine.post('/create-user-for-xibay',value)
-    // .subscribe(
-    //   (data:any) => { this.logs.addLog("Response : "+ JSON.stringify(data))},
-    //   (err:any) => { this.logs.addLog("ERROR : " + JSON.stringify(err))},
-    //   () => { this.logs.addLog("User registered successfully.")}
-    // );
-    // this.networkEngine.createUser(this.defaultFormData)
-    // .subscribe( (data:any) => {
-    //   this.logs.addLog("Response : "+data);
-    //   // console.log(data);
-    // },
-    // (err?:any) => console.log("Error Loging In:" + JSON.stringify(err._body)),
-    // () => { console.log("error") }
-    // );
-
-      this.notify.presentLoading("Please wait...");
-      this.logs.addLog(JSON.stringify(value));
-      this.networkEngine.post(value, "create-user-for-xibay").then((result:string) => {
-        this.responseData = JSON.parse(JSON.parse(result)._body);
-        this.logs.addLog("Response :"+JSON.stringify(this.responseData));
-        console.log(this.responseData);
-        if (this.responseData.User) {
-          this.notify.presentToast("User registered successfully");
-          this.navCtrl.pop();
-        }
-        else {
-          this.notify.presentToast("Please give valid username and password");
-        }
-      }, (err:any) => {
-        //Connection failed message
-        console.log(err.status);
-        if(err.status != 0){
-          this.responseData = JSON.parse(err._body);
-        }else{
-          this.responseData = err;
-        }
-        console.log(this.responseData);
-        if(this.responseData.Error){
-          let error = this.responseData.Error.split(':')[0].split('[')[1].split(']')[0];
-          let field = this.responseData.Error.split('key ')[1];
-          console.log(field);
-          if(error == '23000'){
-            this.notify.presentToast(field + " already registered");    
-          }else if(error == ''){
-          }else if(error == ''){
-          }else if(error == ''){
-          }else if(error == ''){
-
-          }else{
-            this.notify.presentToast("Connection failed");
-          }
-        }else{
-          this.notify.presentToast("Connection failed");
-        }
-        this.notify.closeLoading();
-        // this.notify.presentToast("Connection failed");
-        // this.logs.addLog("Error : " + JSON.stringify(this.responseData));
-      });
-    }
 }

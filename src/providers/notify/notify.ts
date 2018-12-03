e28b5d0f95c60810929498ffce0857483ef9b6a6
MOHAMMED ARSHAD <arshad0141786@gmail.com>
2018-12-04 01:21:09 +0530

    make it better

--- src/providers/notify/notify.ts
+++ src/providers/notify/notify.ts
@@ -47,10 +47,10 @@ export class NotifyProvider {
    presentToast(msg:string) {
     let toast = this.toast.create({
       message: msg,
-      position:'top',
+      position:'middle',
       showCloseButton:true,
       closeButtonText:'OK',
-      cssClass:'toast-danger-css'
+      cssClass:'toast-message'
     });
     console.log("Presenting toast");
     toast.present();
